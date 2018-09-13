77a4a9821b28cbc58b2408fda1c59b5c9eb7da31
Robert Vidal <bobbyvidal@gmail.com>
2018-09-13 15:24:32 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -7,5 +7,5 @@ function destructivelyAddElementToBeginningOfArray(array, element) {
   return array
 }
 function addElementToBeginningOfArray(array, element) {
-  return [array..., element
+  return [array..., element]
 }
